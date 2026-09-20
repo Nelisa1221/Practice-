498c481364aaf44e1b4190a88aff23f44b5269c9
Lisa M <lisamdubo@gmail.com>
2026-09-20 19:13:12 +0200

    Made a new app

--- obj/Release/net10.0-windows/WinFormsApp3.AssemblyInfo.cs
+++ obj/Release/net10.0-windows/WinFormsApp3.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp3")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc22fcd4d0e48baa7ca5c43271c27f91b81fc82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp3")]
